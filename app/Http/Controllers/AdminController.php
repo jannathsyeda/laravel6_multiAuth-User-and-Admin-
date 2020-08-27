039c9aa056dd7e5641ccd78ef4a0d78c14b33656
jannath <jannathsyeda7@gmail.com>
2020-08-27 22:19:06 +0600

    complete user and admin panel all work

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Admin;
+use Auth;
+
+class AdminController extends Controller
+{
+      
+
+       public function __construct()
+    {
+        $this->middleware('auth:admin');
+    }
+
+  public function index(){
+   return view('admin.home');
+  }
+
+
+  public function Logout(){
+   Auth::logout();
+     $notification = array(
+        'message' => 'Admin Successfully Logout',
+        'alert-type' => 'success'
+     );
+
+     return Redirect()->route('admin.login')->with($notification);
+  }
+
+ 
+
+} 
